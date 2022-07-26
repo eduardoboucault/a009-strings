7f8e70749188dfd8db0935a5815a8415736b2d40
Eduardo Boucault <boucault.labenu@outlook.com>
2022-07-26 10:38:02 -0300

    Tarefas realizadas, aguardando feedback.

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+//* Exercício 1
+
+const nome = prompt("Qual é o seu nome?")
+
+const comida1 = prompt("Qual sua comida favorita?")
+
+const comida2 = prompt("Qual sua sobremesa favorita?")
+
+const comida3 = prompt("Qual é a sua bebida favorita?")
+
+console.log(`Estas são as comidas favoritas de ${nome}: \n${comida1} \n${comida2} \n${comida3}`)
